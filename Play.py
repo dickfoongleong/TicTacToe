24e8aea063669af1c1d243812a46ec95d972b83f
DickFoong Leong <dickfoong0314@gmail.com>
2021-02-11 21:52:45 -0500

    Modified to play 1 game.

--- Play.py
+++ Play.py
@@ -8,10 +8,3 @@ while game.state == STATES.CROSS_TURN or game.state == STATES.NAUGHT_TURN:
 	c = random.randint(0,2)
 	game.place_marker(r, c)
 print("Game 1:", game.state.name, sep=' ')
-
-game.restart()
-while game.state == STATES.CROSS_TURN or game.state == STATES.NAUGHT_TURN:
-	r = random.randint(0,2)
-	c = random.randint(0,2)
-	game.place_marker(r, c)
-print("Game 2:", game.state.name, sep=' ')
